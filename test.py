b3a39e2dcd6dbc9fa498819abafefe31be3d91f8
DucAnh2808 <leanht1k59@gmail.com>
2023-09-09 01:11:11 +0700

    k

--- test.py
+++ test.py
@@ -122,5 +122,5 @@ app.register_blueprint(app2_blueprint, url_prefix='/app2')
 app.register_blueprint(app3_blueprint, url_prefix='/app3')
 # server = app.server
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run(port = 8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(port = 8080, debug=False)
